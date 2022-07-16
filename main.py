cca591cd4033e8c78d2407773ee9736d3077e5bb
Victor Le Coz <victor.lecoz@gmail.com>
2022-07-16 18:26:30 +0200

    fix charts of the core-periphery structure

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import sys
 if __name__ == "__main__":
     sys.setrecursionlimit(5000)
     network = ClassNetwork(
-        n_banks=300,
-        alpha_pareto=2.1,
+        n_banks=50,
+        alpha_pareto=1,
         beta_init=0.1,
         beta_reg=0.1,
         beta_star=0.1,
         alpha=0.01,
         gamma=0.03,
         collateral_value=1.0,
-        initialization_method="constant",
-        shock_method="bilateral",
-        shocks_vol=0.1,
+        initialization_method="pareto",
+        shock_method="dirichlet",
+        shocks_vol=0.5,
         result_location="./results/",
     )
 
-    network.simulate(time_steps=5000, save_every=10, jaccard_period=20)
+    network.simulate(
+        time_steps=1000, save_every=100, jaccard_period=20
+    )
